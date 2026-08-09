fb8ddbef8d39a32b16e81c14ddeba1d1b39bf8bd
Edesro-xprox <edesro1996@gmail.com>
2026-08-09 16:57:30 -0500

    Quitar la palabra REACT de las importaciones en el archivo RegisterPage.tsx

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AUTH_SERVICE } from '../services/auth.service';
 
